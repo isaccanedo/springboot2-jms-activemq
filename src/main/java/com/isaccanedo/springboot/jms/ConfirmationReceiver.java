a5635722d299d9c942988999f1a0756b0723cb20
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2022-07-06 11:51:31 -0300

    Add files via upload

---
+++ src/main/java/com/isaccanedo/springboot/jms/ConfirmationReceiver.java
@@ -0,0 +1,18 @@
+package com.isaccanedo.springboot.jms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfirmationReceiver {
+
+    private Logger logger = LoggerFactory.getLogger(ConfirmationReceiver.class);
+
+    @JmsListener(destination = "confirmationQueue", containerFactory = "connectionFactory")
+    public void receiveConfirmation(Confirmation confirmation) {
+        logger.info(" >>  Received confirmation: " + confirmation);
+
+    }
+}
---
+++ src/main/java/com/isaccanedo/springboot/jms/ConfirmationSender.java
@@ -0,0 +1,16 @@
+package com.isaccanedo.springboot.jms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfirmationSender {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    public void sendMessage(final Confirmation confirmation) {
+        jmsTemplate.convertAndSend("confirmationQueue", confirmation);
+    }
+}
